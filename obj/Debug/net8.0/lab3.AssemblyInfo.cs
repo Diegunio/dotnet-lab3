806f9eed0adef2fd6d5655ec27201bf16e1c25c4
Diego Lavayen <diego@example.com>
2025-10-19 09:53:09 +0200

    3.4 - add package, model and index2

--- obj/Debug/net8.0/lab3.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4528d27f3ff6c30b517817ecf0fa90f87e0cce17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a826f3507c0450dbde8bcb0bd0d026986c7bbcf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
